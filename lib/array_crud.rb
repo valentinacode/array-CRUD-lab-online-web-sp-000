7a79f57ed4e2d38bd6dff6249e6d950e8e4cdfea
Valentina E <tiyahelfrih@gmail.com>
2020-07-28 12:37:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,44 +10,42 @@ def create_an_array	def create_an_array
 end	end
 
 
-def add_element_to_end_of_array(array, element)	def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(array, element)
 
   array << element
-end	end
+end
 
 
-def add_element_to_start_of_array(array, element)	def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(array, element)
 
   array.unshift(element)
-end	end
+end
 
 
-def remove_element_from_end_of_array(array)	def remove_element_from_end_of_array(array)
-
+def remove_element_from_end_of_array(array)
   array.pop
-end	end
+end
 
 
-def remove_element_from_start_of_array(array)	def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(array)	
 
   array.shift
 end	end
 
 
-def retreive_element_from_index(array, index_number)	def retreive_element_from_index(array, index_number)
+def retreive_element_from_index(array, index_number)
 
   array[index_number]
-end	end
+end
 
 
-def retreive_first_element_from_array(array)	def retreive_first_element_from_array(array)
+def retreive_first_element_from_array(array)
 
   array[0]
-end	end
+end
 
 
-def retreive_last_element_from_array(array)	def retreive_last_element_from_array(array)
-
+def retreive_last_element_from_array(array)
   x = array.count
   array[x-1]
 end 	end 
