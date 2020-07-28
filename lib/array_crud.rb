b9c5671960d7393b6f23633dd959f31297757d92
Valentina E <tiyahelfrih@gmail.com>
2020-07-28 12:46:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,19 @@ def remove_element_from_start_of_array(array)
 
   array.shift
 end
-def retreive_element_from_index(array, index_number)
-  array[index_number]
-end
+def retrieve_element_from_index(array, index_number)
 
-def retreive_first_element_from_array(array)
 
-  array[0]
+end	end
+
+
+def retreive_first_element_from_array(array)	def retrieve_first_element_from_array(array)
+
+
+end	end
+
+
+def retreive_last_element_from_array(array)	def retrieve_last_element_from_array(array)
+
+
 end
